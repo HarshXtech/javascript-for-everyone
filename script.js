59585c59cdd7d672ee48e875a02f3d64fb1958f6
harsh <harshway08@gmail.com>
2024-09-21 00:24:38 +0530

    revised files

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert("Hello from external file")
+
+// let langauge = "Javascript"
+// let exp = 6
+
+// export {langauge, exp}
+
+
+function greet(){
+    return "Hello from HarshX!"
+
+}
+
+export default greet
+
+// alert()
